521264945b084a84577c8889e872c6635863585e
Dmitry Eremeev <eremeev.dmitry@yandex.ru>
2025-03-11 17:56:19 +0300

    Добавлено решение задачи 01/10-create-app

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,20 @@
-import { defineComponent, createApp } from 'vue'
+import { defineComponent, createApp } from 'vue';
+
+const App = defineComponent({
+  name: 'App',
+
+  setup() {
+    function formatAsLocalDate(timestamp) {
+      return new Date(timestamp).toLocaleString(navigator.language, { dateStyle: 'long' });
+    }
+
+    return {
+      formatAsLocalDate,
+    };
+  },
+
+  template: `Сегодня {{ formatAsLocalDate(new Date()) }}`,
+});
+
+const app = createApp(App);
+app.mount('#app');
