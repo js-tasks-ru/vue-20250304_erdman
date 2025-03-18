ceca1f888d6b94fcf74a18f6593413bfb0814ec0
Dmitry Eremeev <eremeev.dmitry@yandex.ru>
2025-03-18 18:37:32 +0300

    Добавлено решение задачи 03/10-MeetupView

--- 03-components/10-MeetupView/MeetupView.js
+++ 03-components/10-MeetupView/MeetupView.js
@@ -1,10 +1,10 @@
-import { defineComponent } from 'vue'
-import { UiAlert, UiContainer } from '@shgk/vue-course-ui'
-import MeetupAgenda from './MeetupAgenda.js'
-import MeetupDescription from './MeetupDescription.js'
-import MeetupCover from './MeetupCover.js'
-import MeetupInfo from './MeetupInfo.js'
-import './MeetupView.css'
+import { defineComponent, computed } from 'vue';
+import { UiAlert, UiContainer } from '@shgk/vue-course-ui';
+import MeetupAgenda from './MeetupAgenda.js';
+import MeetupDescription from './MeetupDescription.js';
+import MeetupCover from './MeetupCover.js';
+import MeetupInfo from './MeetupInfo.js';
+import './MeetupView.css';
 
 export default defineComponent({
   name: 'MeetupView',
@@ -12,35 +12,50 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupAgenda,
+    MeetupDescription,
+    MeetupCover,
+    MeetupInfo,
+  },
+
+  props: {
+    meetup: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    const hasAgenda = computed(() => props.meetup.agenda?.length > 0);
+
+    return {
+      hasAgenda,
+    };
   },
 
   template: `
     <div>
-
-      <!-- Обложка митапа -->
-
+      <MeetupCover :title="meetup.title" :image="meetup.image" />
       <UiContainer>
         <div class="meetup">
           <div class="meetup__content">
             <h2>Описание</h2>
-
-            <!-- Описание митапа -->
+            <MeetupDescription :description="meetup.description" />
 
             <h2>Программа</h2>
-
-            <!-- Программа митапа -->
-            <!-- Или при пустой программе - сообщение "Программа пока пуста..." в UiAlert -->
-            <UiAlert></UiAlert>
-
+            <MeetupAgenda v-if="hasAgenda" :agenda="meetup.agenda" />
+            <UiAlert v-else>Программа пока пуста...</UiAlert>
           </div>
           <div class="meetup__aside">
-
-            <!-- Краткая информация о митапе -->
-
+            <MeetupInfo
+              :organizer="meetup.organizer"
+              :place="meetup.place"
+              :date="meetup.date"
+            />
             <div class="meetup__aside-buttons"></div>
           </div>
         </div>
       </UiContainer>
     </div>
   `,
-})
+});
